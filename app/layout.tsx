b1aa9cf5128458722f0f7cbf957486b2c2d6e904
oktay <oktaycolakoglu@gmail.com>
2024-06-13 01:25:17 +0300

    eslint prettier configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
